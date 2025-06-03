21ce540f6cc4c4950028bc3a450a5c07d4372ee2
brtiago <tgribeiro@gmail.com>
2025-06-03 12:24:43 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastro_de_jogadores/CadastroDeJogadoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_de_jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeJogadoresApplication.class, args);
+	}
+
+}
