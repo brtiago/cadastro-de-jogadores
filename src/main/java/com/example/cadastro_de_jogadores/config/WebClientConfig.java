57f1c080ac2bb72193cd99b1f3ac665d6c346902
brtiago <tgribeiro@gmail.com>
2025-06-09 14:02:17 -0300

    feat: add Timeouts to WebClientConfig

--- src/main/java/com/example/cadastro_de_jogadores/config/WebClientConfig.java
+++ src/main/java/com/example/cadastro_de_jogadores/config/WebClientConfig.java
@@ -2,15 +2,24 @@ package com.example.cadastro_de_jogadores.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webclient() {
+
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(5));
+
         return WebClient.builder()
                 .baseUrl("https://raw.githubusercontent.com")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
