045aa502cedec95d762d1a6a0ee0066d071f1117
brtiago <tgribeiro@gmail.com>
2025-06-14 09:03:22 -0300

    feat: creating getters and setters

--- src/main/java/com/example/cadastro_de_jogadores/model/Jogador.java
+++ src/main/java/com/example/cadastro_de_jogadores/model/Jogador.java
@@ -6,29 +6,26 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "jogadores")
 public class Jogador {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @NotBlank
-    @Size(max = 20)
+    @NotBlank @Size(max = 20)
     private String nome;
-    @Email
-    @Column(unique = true)
+    @Email @Column(unique = true)
     @Size(max = 50)
     private String email;
 
-    @NotBlank
-    @Column(unique = true)
+    @NotBlank @Column(unique = true)
     @Size(max = 50)
     private String codinome;
     @Size(max = 50)
     private String telefone;
-    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @ManyToOne(fetch = FetchType.LAZY)
     private Grupo grupo;
     @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdAt = LocalDateTime.now();
@@ -48,4 +45,77 @@ public class Jogador {
 
     }
 
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCodinome() {
+        return codinome;
+    }
+
+    public void setCodinome(String codinome) {
+        this.codinome = codinome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Jogador jogador = (Jogador) o;
+        return ativo == jogador.ativo && Objects.equals(id, jogador.id) && Objects.equals(nome, jogador.nome) && Objects.equals(email, jogador.email) && Objects.equals(codinome, jogador.codinome) && Objects.equals(telefone, jogador.telefone) && Objects.equals(grupo, jogador.grupo) && Objects.equals(createdAt, jogador.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, codinome, telefone, grupo, createdAt, ativo);
+    }
 }
