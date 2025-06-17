923f8a4fb4c333d176f088551707daf72b637981
brtiago <tgribeiro@gmail.com>
2025-06-17 15:45:07 -0300

    feat: getters and setters

--- src/main/java/com/example/cadastro_de_jogadores/model/Grupo.java
+++ src/main/java/com/example/cadastro_de_jogadores/model/Grupo.java
@@ -16,4 +16,12 @@ public class Grupo {
     private String nome;
     @Size(max = 100)
     private String descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
