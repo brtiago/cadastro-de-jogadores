669728e1a529f1c1398d0ab32a971ec2d6735e10
brtiago <tgribeiro@gmail.com>
2025-06-18 16:37:04 -0300

    feat: creating CodinomeService

--- src/main/java/com/example/cadastro_de_jogadores/service/JogadorService.java
+++ src/main/java/com/example/cadastro_de_jogadores/service/JogadorService.java
@@ -1,17 +1,11 @@
 package com.example.cadastro_de_jogadores.service;
 
-import com.example.cadastro_de_jogadores.controller.LigaDaJusticaResponse;
-import com.example.cadastro_de_jogadores.controller.VingadorResponse;
 import com.example.cadastro_de_jogadores.model.JogadorDTO;
 import com.example.cadastro_de_jogadores.repository.JogadorRepository;
-import com.example.cadastro_de_jogadores.service.exception.ApiExternaException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
-import java.time.Duration;
 import java.util.List;
 
 @Service
@@ -19,11 +13,11 @@ import java.util.List;
 public class JogadorService {
 
     private final JogadorRepository jogadorRepository;
-    private final WebClient webClient;
+    private final CodinomeService codinomeService;
 
-    public JogadorService(JogadorRepository jogadorRepository, WebClient webclient) {
+    public JogadorService(JogadorRepository jogadorRepository, CodinomeService codinomeService) {
         this.jogadorRepository = jogadorRepository;
-        this.webClient = webclient;
+        this.codinomeService = codinomeService;
     }
 
     public List<JogadorDTO> listarTodos(){
@@ -34,34 +28,5 @@ public class JogadorService {
     }
 
 
-    public Mono<List<String>> getCodinomeLigaDaJustica() {
-        return comResiliencia(
-                webClient.get()
-                        .uri("/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml")
-                        .accept(MediaType.APPLICATION_XML)
-                        .retrieve()
-                        .bodyToMono(LigaDaJusticaResponse.class)
-                        .map(LigaDaJusticaResponse::codinomes)
-        );
-    }
 
-    public Mono<List<String>> getCodinomeVingadores() {
-        return comResiliencia(
-                webClient.get()
-                        .uri("/uolhost/test-backEnd-Java/master/referencias/vingadores.json")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .retrieve()
-                        .bodyToMono(VingadorResponse.class)
-                        .map(VingadorResponse::codinomes)
-        );
-    }
-
-    private <T> Mono<T> comResiliencia(Mono<T> mono) {
-        return mono
-                .timeout(Duration.ofSeconds(3))
-                .onErrorResume(e -> {
-                    log.error("Falha ao buscar codinomes da API externa: {}", e.getMessage());
-                    return Mono.error(new ApiExternaException("Serviço indisponível"));
-                });
-    }
 }
