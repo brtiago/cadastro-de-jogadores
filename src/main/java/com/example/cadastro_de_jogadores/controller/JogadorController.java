c989d46a4942459e9da5da7b69928de4bee1bc34
brtiago <tgribeiro@gmail.com>
2025-06-29 10:32:08 -0300

    feat: creating jogador controller

---
+++ src/main/java/com/example/cadastro_de_jogadores/controller/JogadorController.java
@@ -0,0 +1,33 @@
+package com.example.cadastro_de_jogadores.controller;
+
+import com.example.cadastro_de_jogadores.model.dto.JogadorDTO;
+import com.example.cadastro_de_jogadores.model.dto.JogadorRequest;
+import com.example.cadastro_de_jogadores.service.JogadorService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/jogadores")
+public class JogadorController {
+
+    private final JogadorService jogadorService;
+
+    public JogadorController(JogadorService jogadorService) {
+        this.jogadorService = jogadorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<JogadorDTO>> listarTodos() {
+        return ResponseEntity.ok(jogadorService.listarTodos());
+    }
+
+    @PostMapping
+    public ResponseEntity<JogadorDTO> criar(@Valid @RequestBody JogadorRequest request) {
+        JogadorDTO jogador = jogadorService.criar(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(jogador);
+    }
+}
