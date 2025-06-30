7ab6db3429baa3f05039e1b2790012fe98c5907f
brtiago <tgribeiro@gmail.com>
2025-06-30 16:44:48 -0300

    feat: creating validar email unico method

--- src/main/java/com/example/cadastro_de_jogadores/repository/JogadorRepository.java
+++ src/main/java/com/example/cadastro_de_jogadores/repository/JogadorRepository.java
@@ -4,4 +4,5 @@ import com.example.cadastro_de_jogadores.model.Jogador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JogadorRepository extends JpaRepository<Jogador, Integer> {
+    boolean existsByEmailAndAtivoTrue(String email);
 }
--- src/main/java/com/example/cadastro_de_jogadores/service/JogadorService.java
+++ src/main/java/com/example/cadastro_de_jogadores/service/JogadorService.java
@@ -2,6 +2,7 @@ package com.example.cadastro_de_jogadores.service;
 
 import com.example.cadastro_de_jogadores.model.dto.JogadorDTO;
 import com.example.cadastro_de_jogadores.model.dto.JogadorRequest;
+import com.example.cadastro_de_jogadores.repository.GrupoRepository;
 import com.example.cadastro_de_jogadores.repository.JogadorRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +16,14 @@ public class JogadorService {
 
     private final JogadorRepository jogadorRepository;
     private final CodinomeService codinomeService;
+    private final GrupoRepository grupoRepository;
 
-    public JogadorService(JogadorRepository jogadorRepository, CodinomeService codinomeService) {
+    public JogadorService(JogadorRepository jogadorRepository,
+                          CodinomeService codinomeService,
+                          GrupoRepository grupoRepository) {
         this.jogadorRepository = jogadorRepository;
         this.codinomeService = codinomeService;
+        this.grupoRepository = grupoRepository;
     }
 
     public List<JogadorDTO> listarTodos(){
@@ -32,5 +37,11 @@ public class JogadorService {
     // TODO: fazer método criar
     public JogadorDTO criar(@Valid JogadorRequest request) {
         return null;
+    }
+
+    private void validarEmailUnico(String email) {
+        if (jogadorRepository.existsByEmailAndAtivoTrue(email)) {
+            throw new EmailJaExisteException("Email já está em uso: " + email);
+        }
     }
 }
