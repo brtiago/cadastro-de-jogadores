2d12bab9e4d175f394faad97d313f3e7cd8c61f6
brtiago <tgribeiro@gmail.com>
2025-07-03 11:08:20 -0300

    feat: creating findCodinomeByGrupoNome

--- src/main/java/com/example/cadastro_de_jogadores/repository/JogadorRepository.java
+++ src/main/java/com/example/cadastro_de_jogadores/repository/JogadorRepository.java
@@ -3,6 +3,10 @@ package com.example.cadastro_de_jogadores.repository;
 import com.example.cadastro_de_jogadores.model.Jogador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface JogadorRepository extends JpaRepository<Jogador, Integer> {
     boolean existsByEmailAndAtivoTrue(String email);
+
+    Set<String> findCodinomeByGrupoNome(String ligaDaJustiça);
 }
