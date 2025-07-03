47c4b2d60a70b09cfaa7f2b561e6e1db3ef2bb71
brtiago <tgribeiro@gmail.com>
2025-07-03 11:09:20 -0300

    feat: creating ErrorResponse

---
+++ src/main/java/com/example/cadastro_de_jogadores/service/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.cadastro_de_jogadores.service.exception;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        String codigo,
+        String mensagem,
+        LocalDateTime timestamp,
+        String path
+) {
+    public ErrorResponse(String codigo, String mensagem) {
+        this(codigo, mensagem, LocalDateTime.now(), null);
+    }
+
+    public ErrorResponse(String codigo, String mensagem, String path) {
+        this(codigo, mensagem, LocalDateTime.now(), path);
+    }
+}
