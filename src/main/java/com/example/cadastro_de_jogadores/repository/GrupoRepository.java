11a1ded9e37c56c6e74e1e36a327af85625cb714
brtiago <tgribeiro@gmail.com>
2025-07-03 16:49:20 -0300

    feat: findbynome implemented

--- src/main/java/com/example/cadastro_de_jogadores/repository/GrupoRepository.java
+++ src/main/java/com/example/cadastro_de_jogadores/repository/GrupoRepository.java
@@ -3,5 +3,8 @@ package com.example.cadastro_de_jogadores.repository;
 import com.example.cadastro_de_jogadores.model.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
+    Optional<Grupo> findByNome(String nome);
 }
