fca3e96a46526096dc93c8ed13cd3e88f9437853
brtiago <tgribeiro@gmail.com>
2025-09-10 14:42:10 -0300

    feat: listar, excluir e atualizar metodos

--- src/main/java/com/example/cadastro_de_jogadores/controller/JogadorController.java
+++ src/main/java/com/example/cadastro_de_jogadores/controller/JogadorController.java
@@ -4,6 +4,7 @@ import com.example.cadastro_de_jogadores.model.dto.JogadorRequest;
 import com.example.cadastro_de_jogadores.model.dto.JogadorResponse;
 import com.example.cadastro_de_jogadores.service.JogadorService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,9 +26,28 @@ public class JogadorController {
         return ResponseEntity.ok(jogadorService.listarTodos());
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<JogadorResponse> listarPorId(@PathVariable Integer id) {
+        JogadorResponse jogador = jogadorService.buscarId(id);
+        return ResponseEntity.ok(jogador);
+    }
+
     @PostMapping
     public ResponseEntity<JogadorResponse> criar(@Valid @RequestBody JogadorRequest request) {
         JogadorResponse jogador = jogadorService.criar(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(jogador);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable int id) {
+        jogadorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<JogadorResponse> atualizar(@PathVariable int id, @Valid @RequestBody JogadorRequest request) {
+        JogadorResponse jogador = jogadorService.atualizar(id, request);
+        return ResponseEntity.ok(jogador);
+
     }
 }
